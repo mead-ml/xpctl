cf8f4445d9d1b5bd35d33e2bfbb66dbecdbe5213
sagnik <schoudhury@interactions.com>
2019-05-22 15:15:43 -0400

    move to NodePort

--- orchestration/run-pod.sh
+++ orchestration/run-pod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-K=${K:-kubectl}
+K=kubectl
 
 usage() {
     echo "usage: $0 backend (default mongo) host (default local) port (default 5310)" 1>&2
@@ -8,20 +8,15 @@ usage() {
 
 BACKEND=${1:-mongo}
 HOST=${2:-local}
-PORT=${3:-5310}
-if [[ -z "${BACKEND//}" || -z "${HOST//}" || -z "${PORT//}" ]];
+DOCKER_COMMAND=${3:-docker}
+if [[ -z "${BACKEND//}" || -z "${HOST//}" || -z "${DOCKER_COMMAND//}" ]];
 then
   usage
   exit 1
 fi
 
-if [[ ${K} == 'microk8s.kubectl' ]];
-then
-  DOCKER_COMMAND=microk8s.docker
-else
-  DOCKER_COMMAND=docker
-fi
+
 
 ./build-docker.sh ${BACKEND} ${HOST} ${DOCKER_COMMAND}
 ${K} apply -f k8s/"$BACKEND"/ &
-${K} port-forward svc/xpctl-server 5310:5310 &
+#${K} port-forward svc/xpctl-server 5310:5310 &
